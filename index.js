6b32627187034b11ad20698e4d36b68b776b83b6
Urmi-Jana <janaurmi6@gmail.com>
2023-06-05 17:57:54 +0530

    add save and change key functionalities

---
+++ index.js
@@ -0,0 +1,14 @@
+//add event listeners
+
+const saveKey = () => {
+    
+}
+const changeKey = () => {}
+
+//for saving the key button
+document.getElementById("save_key_button").addEventListener('click', saveKey);
+
+//for the change key button
+document.getElementById("change_key_button").addEventListener('click', changeKey);
+
+
