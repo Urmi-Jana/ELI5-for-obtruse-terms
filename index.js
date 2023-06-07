1a80c6d6f4f4c09bd1fa1769214b6095dd404daf
Urmi-Jana <janaurmi6@gmail.com>
2023-06-07 13:12:23 +0530

    add save and change keys

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 //add event listeners
 
-const saveKey = () => {
-    
+const encode = (val) => {
+    return btoa(val)
 }
-const changeKey = () => {}
+//when key is to be saved
+const saveKey = () => {
+    const input = document.getElementById('key_input');
+
+    if (input){
+        const {value} = input;
+    
+
+        //key encoding
+        const encodedVal = encode(value);
+
+        //saving to google storage
+        chrome.storage.local.set({ 'openai-key': encodedValue }, () => {
+            document.getElementById('key_needed').style.display = 'none';
+            document.getElementById('key_entered').style.display = 'block';
+        });
+    }
+}
+
+
+const changeKey = () => {
+    document.getElementById('key_needed').style.display = 'none';
+    document.getElementById('key_entered').style.display = 'block';
+
+}
 
 //for saving the key button
 document.getElementById("save_key_button").addEventListener('click', saveKey);
