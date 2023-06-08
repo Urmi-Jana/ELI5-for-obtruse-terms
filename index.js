6bb8316f2757a97fb6ecbce5c94753934d3d7c5a
Urmi-Jana <janaurmi6@gmail.com>
2023-06-08 22:17:50 +0530

    check if key is present

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 //add event listeners
 
+//check for key
+
+const checkKey = () => {
+
+    //use promise to make sure that callback is resolved before returning
+
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(['openai-key'], (result) => {
+            resolve(result['openai-key']);
+        })
+    })
+}
+
 const encode = (val) => {
     return btoa(val)
 }
@@ -10,9 +23,8 @@ const saveKey = () => {
     if (input){
         const {value} = input;
     
-
         //key encoding
-        const encodedVal = encode(value);
+        const encodedValue = encode(value);
 
         //saving to google storage
         chrome.storage.local.set({ 'openai-key': encodedValue }, () => {
@@ -22,10 +34,9 @@ const saveKey = () => {
     }
 }
 
-
 const changeKey = () => {
-    document.getElementById('key_needed').style.display = 'none';
-    document.getElementById('key_entered').style.display = 'block';
+    document.getElementById('key_needed').style.display = 'block';
+    document.getElementById('key_entered').style.display = 'none';
 
 }
 
@@ -36,3 +47,10 @@ document.getElementById("save_key_button").addEventListener('click', saveKey);
 document.getElementById("change_key_button").addEventListener('click', changeKey);
 
 
+//then clause for promis = always runs when ext is opened
+checkKey().then((response) => {
+    if (response) {
+      document.getElementById('key_needed').style.display = 'none';
+      //document.getElementById('key_entered').style.display = 'block';
+    }
+  });
