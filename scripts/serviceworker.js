2347cc176e8ab3516e36ddc30f9f060b2c81627e
Urmi-Jana <janaurmi6@gmail.com>
2023-06-10 21:48:14 +0530

    add OpenAI API

--- scripts/serviceworker.js
+++ scripts/serviceworker.js
@@ -1,6 +1,43 @@
 //highlight text in pdf, right click, generate info
 
+//to get and decode key
+const getKey = () => {
+    return new Promise ((resolve, reject) => {
+        chrome.storage.local.get(['openai-key'], (result) => {
+            if (result['openai-key']){
+                const key = (atob(result['openai-key']));
+                resolve(key)
+            };
+        })
+    })
+}
 
+//generate output
+const generate = async (prompt) => {
+    const key = await getKey(); //get key from storage
+    const url = 'https://api.openai.com/v1/chat/completions';
+
+    const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${key}`,
+            },
+            body: JSON.stringify({
+            model: 'text-davinci-003',
+            prompt: prompt,
+            max_tokens: 1250,
+            temperature: 0.7,
+        }),
+    })
+    console.log(response);
+
+    const output = await response.json();
+    return output.text;
+    
+}
+
+//after clicking
 const generateCompletionAction = async (info) => {
     try{
         const {selectedText} = info;
@@ -9,6 +46,9 @@ const generateCompletionAction = async (info) => {
 
             Term: 
         `;
+
+        const completePrompt = await generate (`${basePrompt}${selectedText}`);
+        console.log(completePrompt.text);
     }catch(error){
         console.log(error);
     }
